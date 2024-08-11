9c14b51c13fa0531bd746e8b95bf864a933cd4ef
priyaGangwar <priyag21@iitk.ac.in>
2024-08-11 16:40:53 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const container = document.querySelector(".grid-container");
+let gameCards = [];
+let firstSelection, secondSelection;
+let isBoardLocked = false;
+let currentScore = 0;
+
+document.querySelector(".score").textContent = currentScore;
+
+fetch("./data/cards.json")
+  .then((response) => response.json())
+  .then((cardData) => {
+    gameCards = [...cardData, ...cardData];
+    randomizeCards();
+    createCards();
+  });
+
+function randomizeCards() {
+  let remainingCards = gameCards.length,
+      randomIndex,
+      tempValue;
+
+  while (remainingCards > 0) {
+    randomIndex = Math.floor(Math.random() * remainingCards);
+    remainingCards--;
+
+    tempValue = gameCards[remainingCards];
+    gameCards[remainingCards] = gameCards[randomIndex];
+    gameCards[randomIndex] = tempValue;
+  }
+}
+
+function createCards() {
+  gameCards.forEach((card) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardDiv.setAttribute("data-card", card.name);
+    cardDiv.innerHTML = `
+      <div class="front">
+        <img class="front-image" src=${card.image} />
+      </div>
+      <div class="back"></div>
+    `;
+    container.appendChild(cardDiv);
+    cardDiv.addEventListener("click", handleCardClick);
+  });
+}
+
+function handleCardClick() {
+  if (isBoardLocked) return;
+  if (this === firstSelection) return;
+
+  this.classList.add("flipped");
+
+  if (!firstSelection) {
+    firstSelection = this;
+    return;
+  }
+
+  secondSelection = this;
+  currentScore++;
+  document.querySelector(".score").textContent = currentScore;
+  isBoardLocked = true;
+
+  checkForCardMatch();
+}
+
+function checkForCardMatch() {
+  const isMatchingPair = firstSelection.dataset.card === secondSelection.dataset.card;
+
+  isMatchingPair ? disableSelectedCards() : resetFlippedCards();
+}
+
+function disableSelectedCards() {
+  firstSelection.removeEventListener("click", handleCardClick);
+  secondSelection.removeEventListener("click", handleCardClick);
+
+  resetGameBoard();
+}
+
+function resetFlippedCards() {
+  setTimeout(() => {
+    firstSelection.classList.remove("flipped");
+    secondSelection.classList.remove("flipped");
+    resetGameBoard();
+  }, 1000);
+}
+
+function resetGameBoard() {
+  [firstSelection, secondSelection, isBoardLocked] = [null, null, false];
+}
+
+function restartGame() {
+  resetGameBoard();
+  randomizeCards();
+  currentScore = 0;
+  document.querySelector(".score").textContent = currentScore;
+  container.innerHTML = "";
+  createCards();
+}
