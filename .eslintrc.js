976ae0fa20f1305eec89994f16ee127d68e24dd9
priyaGangwar <priyag21@iitk.ac.in>
2024-09-12 20:30:34 +0530

    added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
-{
-  "env": {
-    "browser": true,
-    "es6": true
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 2015,
-    "sourceType": "module"
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: 'module',
   },
-  "plugins": ["complexity", "no-duplicate-imports"],
-  "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single"],
-    "semi": ["error", "always"],
-    "complexity": ["error", { "max": 10 }],
-    "no-duplicate-imports": "error"
-  }
-}
-
+  plugins: ['complexity', 'no-duplicate-imports'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    complexity: ['error', { max: 10 }],
+    'no-duplicate-imports': 'error',
+  },
+};
