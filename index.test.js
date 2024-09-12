c99d367eaa7a87837042105346137132e59fe679
priyaGangwar <priyag21@iitk.ac.in>
2024-09-12 20:15:58 +0530

    added

--- index.test.js
+++ index.test.js
@@ -7,3 +7,10 @@ describe("Utility tests", () => {
         expect(cards).not.toEqual([{ name: 'A' }, { name: 'B' }]);
     });
 });
+describe("Utility tests", () => {
+  test("Reloads only when needed", function () {
+      return __awaiter(this, void 0, void 0, function* () {
+          //  let cm = new cmake.CMakeHelpers();
+      });
+  });
+});
