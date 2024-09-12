518e5a05521e7b115a759a2ca5a92036535f3c11
priyaGangwar <priyag21@iitk.ac.in>
2024-09-12 19:24:25 +0530

    change in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'jsx'],
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  };
-  
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
